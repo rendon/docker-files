a3a21084eb761f28813873372bfa1228f8e4e15b
Rafael Rendon Pablo <rafaelrendonpablo@gmail.com>
2024-04-23 18:06:43 -0700

    Remove repeated flags

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -4,4 +4,4 @@ docker run -d\
     --name mysql-container\
     -e MYSQL_ROOT_PASSWORD=$MYSQL_PASSWORD\
     -p 3306:3306\
-    -d mysql:8.3.0
+    mysql:8.3.0
--- postgres/setup.sh
+++ postgres/setup.sh
@@ -4,5 +4,4 @@ docker run -d\
     --name postgres-container\
     -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD\
     -p 5432:5432\
-    -d postgres:15
-
+    postgres:15
